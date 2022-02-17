4b3b8a197eea331ac0bbb8a61abab7a96fe784d2
Kargin Daniil <daniilus.k@gmail.com>
2022-02-17 13:09:27 +0300

    telegram_without_token

--- лаб1/main.py
+++ лаб1/main.py
@@ -1,3 +1,30 @@
-name = 'daniil'
-age = 18
-print(name, age)
+# Вариант 1 - самый простой чат бот, просто отзывается
+
+import telebot  # pyTelegramBotAPI	4.3.1
+
+bot = telebot.TeleBot('5187062324:AAHbopf8CawQfNbKYh9DT0SYW-befNjMSNk')  # Создаем экземпляр бота @Ivanov_Ivan_1MD19_bot
+
+# -----------------------------------------------------------------------
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(message, res=False):
+    chat_id = message.chat.id
+
+    bot.send_message(chat_id,
+                     text="Привет, {0.first_name}! Я тестовый бот для курса программирования на языке ПаЙтон".format(
+                         message.from_user))
+
+
+# -----------------------------------------------------------------------
+# Получение сообщений от юзера
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    chat_id = message.chat.id
+    ms_text = message.text
+    bot.send_message(chat_id, text="Я тебя слышу!!! Ваше сообщение: " + ms_text)
+
+
+# -----------------------------------------------------------------------
+bot.polling(none_stop=True, interval=0) # Запускаем бота
+
+print()
